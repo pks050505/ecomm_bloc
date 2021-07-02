126f7b8a335df64d0dd5a514fffba02f937d9abe
Purushottam kumar <pks050505@gmail.com>
2021-07-01 23:20:45 -0700

    Added some changes

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce/model/model.dart';
+import 'package:ecommerce/screens/screens.dart';
 import 'package:flutter/material.dart';
 
 class ProductCard extends StatelessWidget {
@@ -10,73 +11,78 @@ class ProductCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final double widthValue = MediaQuery.of(context).size.width / widthFactor!;
-    return Stack(
-      children: [
-        Container(
-          width: widthValue,
-          height: 150,
-          child: Image.network(
-            product.imageUrl,
-            fit: BoxFit.cover,
-          ),
-        ),
-        Positioned(
-          top: 60,
-          child: Container(
+    return InkWell(
+      onTap: () {
+        Navigator.pushNamed(context, ProductScreen.routeName);
+      },
+      child: Stack(
+        children: [
+          Container(
             width: widthValue,
-            height: 80,
-            decoration: BoxDecoration(color: Colors.black.withAlpha(50)),
-          ),
-        ),
-        Positioned(
-          top: 60,
-          left: 5,
-          child: Container(
-            height: 80,
-            width: widthValue - 10,
-            decoration: BoxDecoration(color: Colors.black),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Expanded(
-                    flex: 3,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          product.name!,
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline5!
-                              .copyWith(color: Colors.white),
-                        ),
-                        Text(
-                          '\$${product.price}',
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline6!
-                              .copyWith(color: Colors.white),
-                        )
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    child: IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.add_circle,
-                        color: Colors.white,
-                      ),
-                    ),
-                  )
-                ],
-              ),
+            height: 150,
+            child: Image.network(
+              product.imageUrl,
+              fit: BoxFit.cover,
             ),
           ),
-        )
-      ],
+          Positioned(
+            top: 60,
+            child: Container(
+              width: widthValue,
+              height: 80,
+              decoration: BoxDecoration(color: Colors.black.withAlpha(50)),
+            ),
+          ),
+          Positioned(
+            top: 60,
+            left: 5,
+            child: Container(
+              height: 80,
+              width: widthValue - 10,
+              decoration: BoxDecoration(color: Colors.black),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 3,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            product.name!,
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline5!
+                                .copyWith(color: Colors.white),
+                          ),
+                          Text(
+                            '\$${product.price}',
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline6!
+                                .copyWith(color: Colors.white),
+                          )
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.add_circle,
+                          color: Colors.white,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
