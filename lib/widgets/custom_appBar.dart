b826d83c7b4957cc4ab171331d51f5ba7a5c0014
Purushottam kumar <pks050505@gmail.com>
2021-07-02 18:15:49 -0700

    path / remove

--- lib/widgets/custom_appBar.dart
+++ lib/widgets/custom_appBar.dart
@@ -9,6 +9,9 @@ class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
   final String? title;
   @override
   Widget build(BuildContext context) {
+    var screenHeight = MediaQuery.of(context).size.height;
+    // screen width
+    var screenWidth = MediaQuery.of(context).size.width;
     return AppBar(
       centerTitle: true,
       backgroundColor: Colors.black12,
